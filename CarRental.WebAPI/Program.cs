52fd1d94c0eb39bcdefaf7a37d22dda52b6b6207
lfuschetto <leandro.fuschetto@dataart.com>
2022-09-11 13:20:30 -0300

    configure automapper, sql context in program

--- CarRental.WebAPI/Program.cs
+++ CarRental.WebAPI/Program.cs
@@ -1,13 +1,35 @@
+using AutoMapper;
+using CarRental.Data;
+using CarRental.Mapper;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var config = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new MapperProfile());
+});
+var mapper = config.CreateMapper();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddDbContext<CarRentalContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<CarRentalContext>();
+    context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,9 +39,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

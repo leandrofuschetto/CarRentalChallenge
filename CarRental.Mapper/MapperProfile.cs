aee7d7b94640e7482e8a5d1b7fd30dfa63b9d080
lfuschetto <leandro.fuschetto@dataart.com>
2022-09-11 13:15:44 -0300

    add mapper project with AutoMapper

---
+++ CarRental.Mapper/MapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CarRental.Data.Entities;
+using CarRental.Domain.Models;
+
+namespace CarRental.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Client, ClientEntity>();
+            CreateMap<ClientEntity, Client>();
+
+            CreateMap<Vehicle, VehicleEntity>();
+            CreateMap<VehicleEntity, Vehicle>();
+
+            CreateMap<Rental, RentalEntity>();
+            CreateMap<RentalEntity, Rental>();
+        }
+    }
+}
